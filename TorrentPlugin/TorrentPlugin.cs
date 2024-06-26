21b210cf6c3cad0747eaa2de901e0c5c5d3229a5
IsraelCohen <il90il90@Gmail.com>
2024-06-26 19:49:59 +0300

    add plugin without implement torrent

---
+++ TorrentPlugin/TorrentPlugin.cs
@@ -0,0 +1,20 @@
+﻿using BasePlugins;
+
+namespace TorrentPlugin
+{
+    public class TorrentPlugin<TMessage> : BasePlugin<TMessage>
+    {
+        public override string PluginName => throw new NotImplementedException();
+
+        public override bool CanHandle(Config config)
+        {
+            return config.Text.StartsWith("magnet:");
+        }
+
+        public override async Task<ResultExecute> ExecuteAsync(Config config)
+        {
+            Console.WriteLine("Download complete!");
+            return new ResultExecute();
+        }
+    }
+}
